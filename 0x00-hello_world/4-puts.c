6145ea663da0a89ea2d5754135b257db0d841c7c
George Kule <georgekule@gmail.com>
2022-10-13 16:19:31 -0700

    Third Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle"
  *
  *   * Return: 0 if exited properly, non-zero otherwise
  *
@@ -14,8 +14,7 @@
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
